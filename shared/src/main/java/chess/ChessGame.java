963f5fca0998852e8b28261105d8eb006386fbbf
wypoch <wywyt213@gmail.com>
2025-09-15 13:29:17 -0600

    Implement getters and setters in ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,9 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private TeamColor currTeam;
+    private ChessBoard currBoard;
+
     public ChessGame() {
 
     }
@@ -28,7 +31,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return currTeam;
     }
 
     /**
@@ -37,7 +40,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        var currTeam = team;
     }
 
     /**
@@ -106,7 +109,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        var currBoard = board;
     }
 
     /**
@@ -115,6 +118,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return currBoard;
     }
 }
