691552e8c0014a4586ce1b85c58859d609bbf0e5
wypoch <wywyt213@gmail.com>
2025-09-29 17:07:13 -0600

    Implement inheritance to make the ChessPiece class more logical

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -60,26 +60,176 @@ public class ChessPiece {
         return type;
     }
 
-    /**
-     * Takes an existing move set and board, and adds the possible moves for a piece of the indicated color
-     * starting from the indicated position. NOTE: does not consider whether a move leaves a king in danger.
-     *
-     * @param moves existing move set
-     * @param startPos position of the piece
-     * @param board existing board
-     * @param myColor color of the piece
-     */
-    public void addMovesBase(HashSet<ChessMove> moves, ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor,
-                             int[][] offsets, boolean recursive) {
+    static public class BaseMoves {
 
-        for (var offset : offsets) {
-            int numIters = 0;
+        /**
+         * Takes an existing board, and adds the possible moves for a piece of the indicated color starting from the indicated position.
+         * The piece moves in coordinate steps included in the offsets list, with optional recursion.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos  position of the piece
+         * @param board     existing board
+         * @param myColor   color of the piece
+         * @param offsets   directions the piece can move
+         * @param recursive whether the piece can make multiple moves in the direction of an offset
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesBase(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor,
+                                 int[][] offsets, boolean recursive) {
+
+            HashSet<ChessMove> moves = new HashSet<>();
+
+            for (var offset : offsets) {
+                int numIters = 0;
+                var currRow = startPos.getRow();
+                var currCol = startPos.getColumn();
+
+                while (true) {
+                    currRow += offset[0];
+                    currCol += offset[1];
+
+                    if (currRow < 1 || currRow > 8 || currCol < 1 || currCol > 8) {
+                        break;
+                    }
+
+                    var newPos = new ChessPosition(currRow, currCol);
+                    var newPiece = board.getPiece(newPos);
+                    if (newPiece != null) {
+                        if (newPiece.getTeamColor() != myColor) {
+                            moves.add(new ChessMove(startPos, newPos, null));
+                        }
+                        break;
+                    }
+                    moves.add(new ChessMove(startPos, newPos, null));
+                    numIters += 1;
+                    if (numIters == 1 && !recursive) {
+                        break;
+                    }
+                }
+            }
+            return moves;
+        }
+    }
+
+    static public class BishopMoves extends BaseMoves {
+        /**
+         * Takes an existing board, and adds the possible moves for a bishop of the indicated color starting from the indicated position.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos position of the piece
+         * @param board    existing board
+         * @param myColor  color of the piece
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesBishop(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
+            int[][] bishopOffsets = new int[][]{{1, -1}, {-1, 1}, {1, 1}, {-1, -1}};
+            return computeMovesBase(startPos, board, myColor, bishopOffsets, true);
+        }
+    }
+
+    static public class KingMoves extends BaseMoves {
+        /**
+         * Takes an existing board, and adds the possible moves for a king of the indicated color starting from the indicated position.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos position of the piece
+         * @param board    existing board
+         * @param myColor  color of the piece
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesKing(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
+            int[][] kingOffsets = new int[][]{{1, -1}, {-1, 1}, {1, 1}, {-1, -1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            return computeMovesBase(startPos, board, myColor, kingOffsets, false);
+        }
+    }
+
+    static public class KnightMoves extends BaseMoves {
+        /**
+         * Takes an existing board, and adds the possible moves for a knight of the indicated color starting from the indicated position.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos position of the piece
+         * @param board    existing board
+         * @param myColor  color of the piece
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesKnight(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
+            int[][] knightOffsets = new int[][]{{2, -1}, {-2, 1}, {2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, -2}, {-1, 2}};
+            return computeMovesBase(startPos, board, myColor, knightOffsets, false);
+        }
+    }
+
+    static public class RookMoves extends BaseMoves {
+        /**
+         * Takes an existing board, and adds the possible moves for a rook of the indicated color starting from the indicated position.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos position of the piece
+         * @param board    existing board
+         * @param myColor  color of the piece
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesRook(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
+            int[][] rookOffsets = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            return computeMovesBase(startPos, board, myColor, rookOffsets, true);
+        }
+    }
+
+    static public class QueenMoves extends BaseMoves {
+        /**
+         * Takes an existing board, and adds the possible moves for a queen of the indicated color starting from the indicated position.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos position of the piece
+         * @param board    existing board
+         * @param myColor  color of the piece
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesQueen(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
+            int[][] queenOffsets = new int[][]{{1, -1}, {-1, 1}, {1, 1}, {-1, -1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            return computeMovesBase(startPos, board, myColor, queenOffsets, true);
+        }
+    }
+
+    static public class PawnMoves {
+        /**
+         * Takes an existing move set and board, and adds the possible moves for a pawn of the indicated color starting from the indicated position.
+         * NOTE: does not consider whether a move leaves a king in danger.
+         *
+         * @param startPos position of the pawn
+         * @param board existing board
+         * @param myColor color of the pawn
+         *
+         * @return Collection of valid moves
+         */
+        public HashSet<ChessMove> computeMovesPawn(ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
+            HashSet<ChessMove> moves = new HashSet<>();
+
+            ChessPiece.PieceType[] promotionPieces = new ChessPiece.PieceType[]{PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, PieceType.QUEEN};
+
+            int maxMoves = 1;
             var currRow = startPos.getRow();
             var currCol = startPos.getColumn();
 
-            while (true) {
-                currRow += offset[0];
-                currCol += offset[1];
+            if (myColor == ChessGame.TeamColor.WHITE && currRow == 2) {
+                maxMoves = 2;
+            } else if (myColor == ChessGame.TeamColor.BLACK && currRow == 7) {
+                maxMoves = 2;
+            }
+
+            // Forward moves
+            while (maxMoves > 0) {
+                if (myColor == ChessGame.TeamColor.WHITE) {
+                    currRow += 1;
+                } else if (myColor == ChessGame.TeamColor.BLACK) {
+                    currRow -= 1;
+                }
 
                 if (currRow < 1 || currRow > 8 || currCol < 1 || currCol > 8) {
                     break;
@@ -88,106 +238,55 @@ public class ChessPiece {
                 var newPos = new ChessPosition(currRow, currCol);
                 var newPiece = board.getPiece(newPos);
                 if (newPiece != null ) {
-                    if (newPiece.getTeamColor() != myColor) {
-                        moves.add(new ChessMove(startPos, newPos, null));
+                    break;
+                }
+
+                if ((myColor == ChessGame.TeamColor.WHITE && currRow == 8) || (myColor == ChessGame.TeamColor.BLACK && currRow == 1)) {
+                    for (var pPiece : promotionPieces) {
+                        moves.add(new ChessMove(startPos, newPos, pPiece));
                     }
-                    break;
+                } else {
+                    moves.add(new ChessMove(startPos, newPos, null));
                 }
-                moves.add(new ChessMove(startPos, newPos, null));
-                numIters += 1;
-                if (numIters == 1 && !recursive) {
-                    break;
-                }
+                maxMoves -= 1;
             }
-        }
-    }
 
-    /**
-     * Takes an existing move set and board, and adds the possible moves for a pawn of the indicated color
-     * starting from the indicated position. NOTE: does not consider whether a move leaves a king in danger.
-     *
-     * @param moves existing move set
-     * @param startPos position of the pawn
-     * @param board existing board
-     * @param myColor color of the pawn
-     */
-    public void addMovesPawn(HashSet<ChessMove> moves, ChessPosition startPos, ChessBoard board, ChessGame.TeamColor myColor) {
-
-        ChessPiece.PieceType[] promotionPieces = new ChessPiece.PieceType[]{PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, PieceType.QUEEN};
-
-        int maxMoves = 1;
-        var currRow = startPos.getRow();
-        var currCol = startPos.getColumn();
-
-        if (myColor == ChessGame.TeamColor.WHITE && currRow == 2) {
-            maxMoves = 2;
-        } else if (myColor == ChessGame.TeamColor.BLACK && currRow == 7) {
-            maxMoves = 2;
-        }
-
-        // Forward moves
-        while (maxMoves > 0) {
+            // Diagonal moves
+            int[][] offsets;
             if (myColor == ChessGame.TeamColor.WHITE) {
-                currRow += 1;
-            } else if (myColor == ChessGame.TeamColor.BLACK) {
-                currRow -= 1;
-            }
-
-            if (currRow < 1 || currRow > 8 || currCol < 1 || currCol > 8) {
-                break;
-            }
-
-            var newPos = new ChessPosition(currRow, currCol);
-            var newPiece = board.getPiece(newPos);
-            if (newPiece != null ) {
-                break;
-            }
-
-            if ((myColor == ChessGame.TeamColor.WHITE && currRow == 8) || (myColor == ChessGame.TeamColor.BLACK && currRow == 1)) {
-                for (var pPiece : promotionPieces) {
-                    moves.add(new ChessMove(startPos, newPos, pPiece));
-                }
+                offsets = new int[][]{{1, -1}, {1, 1}};
             } else {
-                moves.add(new ChessMove(startPos, newPos, null));
-            }
-            maxMoves -= 1;
-        }
-
-        // Diagonal moves
-        int[][] offsets;
-        if (myColor == ChessGame.TeamColor.WHITE) {
-            offsets = new int[][]{{1, -1}, {1, 1}};
-        } else {
-            offsets = new int[][]{{-1, -1}, {-1, 1}};
-        }
-
-        for (var offset : offsets) {
-            var currRow2 = startPos.getRow();
-            var currCol2 = startPos.getColumn();
-
-            currRow2 += offset[0];
-            currCol2 += offset[1];
-
-            if (currRow2 < 1 || currRow2 > 8 || currCol2 < 1 || currCol2 > 8) {
-                continue;
+                offsets = new int[][]{{-1, -1}, {-1, 1}};
             }
 
-            var newPos = new ChessPosition(currRow2, currCol2);
-            var newPiece = board.getPiece(newPos);
-            if (newPiece != null) {
-                if (newPiece.getTeamColor() != myColor) {
-                    if ((myColor == ChessGame.TeamColor.WHITE && currRow2 == 8) || (myColor == ChessGame.TeamColor.BLACK && currRow2 == 1)) {
-                        for (var pPiece : promotionPieces) {
-                            moves.add(new ChessMove(startPos, newPos, pPiece));
+            for (var offset : offsets) {
+                var currRow2 = startPos.getRow();
+                var currCol2 = startPos.getColumn();
+
+                currRow2 += offset[0];
+                currCol2 += offset[1];
+
+                if (currRow2 < 1 || currRow2 > 8 || currCol2 < 1 || currCol2 > 8) {
+                    continue;
+                }
+
+                var newPos = new ChessPosition(currRow2, currCol2);
+                var newPiece = board.getPiece(newPos);
+                if (newPiece != null) {
+                    if (newPiece.getTeamColor() != myColor) {
+                        if ((myColor == ChessGame.TeamColor.WHITE && currRow2 == 8) || (myColor == ChessGame.TeamColor.BLACK && currRow2 == 1)) {
+                            for (var pPiece : promotionPieces) {
+                                moves.add(new ChessMove(startPos, newPos, pPiece));
+                            }
+                        } else {
+                            moves.add(new ChessMove(startPos, newPos, null));
                         }
-                    } else {
-                        moves.add(new ChessMove(startPos, newPos, null));
-                    }
 
+                    }
                 }
             }
+            return moves;
         }
-
     }
 
     /**
@@ -198,42 +297,16 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        HashSet<ChessMove> moves = new HashSet<>();
         ChessPiece currPiece = board.getPiece(myPosition);
         var myColor = currPiece.getTeamColor();
 
-        switch (currPiece.getPieceType()) {
-            case PieceType.BISHOP:
-                addMovesBase(moves, myPosition, board, myColor, new int[][]{{1, -1}, {-1, 1}, {1, 1}, {-1, -1}}, true);
-                break;
-
-            case PieceType.KING:
-                addMovesBase(moves, myPosition, board, myColor,
-                        new int[][]{{1, -1}, {-1, 1}, {1, 1}, {-1, -1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}}, false);
-                break;
-
-            case PieceType.KNIGHT:
-                addMovesBase(moves, myPosition, board, myColor,
-                        new int[][]{{2, -1}, {-2, 1}, {2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, -2}, {-1, 2}}, false);
-                break;
-
-            case PieceType.ROOK:
-                addMovesBase(moves, myPosition, board, myColor,
-                        new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}, true);
-                break;
-
-            case PieceType.QUEEN:
-                addMovesBase(moves, myPosition, board, myColor,
-                        new int[][]{{1, -1}, {-1, 1}, {1, 1}, {-1, -1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}}, true);
-                break;
-
-            case PieceType.PAWN:
-                addMovesPawn(moves, myPosition, board, myColor);
-                break;
-
-            default:
-                break;
-        }
-        return moves;
+        return switch (currPiece.getPieceType()) {
+            case PieceType.BISHOP -> new BishopMoves().computeMovesBishop(myPosition, board, myColor);
+            case PieceType.KING -> new KingMoves().computeMovesKing(myPosition, board, myColor);
+            case PieceType.KNIGHT -> new KnightMoves().computeMovesKnight(myPosition, board, myColor);
+            case PieceType.ROOK -> new RookMoves().computeMovesRook(myPosition, board, myColor);
+            case PieceType.QUEEN -> new QueenMoves().computeMovesQueen(myPosition, board, myColor);
+            case PieceType.PAWN -> new PawnMoves().computeMovesPawn(myPosition, board, myColor);
+        };
     }
 }
